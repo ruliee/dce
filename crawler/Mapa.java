ef7efa00b80d007b1ea1b915c42d8be6d5023654
raulvidal228 <raulvidal228@32412616-0637-11df-bdae-5552c88b53c9>
2010-02-07 15:49:55 +0000

    ya serializa

--- crawler/Mapa.java
+++ crawler/Mapa.java
@@ -1,5 +1,11 @@
 package crawler;
 
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class Mapa {
 	Cuarto [][][]dungeon;
 	/*
@@ -10,5 +16,39 @@ public class Mapa {
 	//TODO: hacer que cargue el dungeon de un archivo.
 	public Cuarto dameelCuarto(int x,int y,int z){
 		return dungeon[x][y][z];
+	}
+	private void guardaCuartos(String file) {
+		
+		try {
+			FileOutputStream os = new FileOutputStream(file);
+			XMLEncoder encoder = new XMLEncoder(os);
+			Mapa m = this;
+			encoder.writeObject(m);
+			encoder.close(); 
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("se jodieron los archivos");
+		}
+		
+
+	}
+	private Mapa cargaCuartos(String file) {
+		
+		try {
+			FileInputStream os = new FileInputStream(file);
+			XMLDecoder decoder = new XMLDecoder(os);
+			Mapa m = (Mapa)decoder.readObject();
+			decoder.close();
+			return m;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("coño los archivos no aparecen");
+			return null;
+		}
+		 
+	
+
 	}
 }
